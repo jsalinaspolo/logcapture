64ca0bd3b80046732ab319540c9817ebc74659d8
MuSTa1nE <mustainer@gmail.com>
2020-03-27 23:24:50 +0100

    Make logcapgture modular

---
+++ logcapture-spock/src/main/groovy/com/logcapture/spock/LogCaptureTrait.groovy
@@ -0,0 +1,34 @@
+package com.logcapture.spock
+
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.ILoggingEvent
+import com.logcapture.LogCapture
+import com.logcapture.logback.StubAppender
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Before
+import org.slf4j.LoggerFactory
+
+trait LogCaptureTrait {
+
+  def root
+  def logAppender
+
+  @Before
+  def setupLogger() {
+    logAppender = new StubAppender()
+    root = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
+
+    root.addAppender(logAppender)
+  }
+
+  @After
+  def detachAppender() {
+    root.detachAppender(logAppender)
+  }
+
+  LogCaptureTrait logged(Matcher<List<ILoggingEvent>> expectedLoggingMessage) {
+    new LogCapture<>(logAppender.events()).logged(expectedLoggingMessage)
+    return this
+  }
+}
---
+++ logcapture-spock/src/test/groovy/com/logcapture/spock/LogCaptureTraitShould.groovy
@@ -0,0 +1,40 @@
+package com.logcapture.spock
+
+import org.slf4j.LoggerFactory
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.INFO
+import static com.logcapture.assertion.ExpectedLoggingMessage.aLog
+import static org.hamcrest.Matchers.*
+
+class LogCaptureTraitShould extends Specification implements LogCaptureTrait {
+
+  @Shared log = LoggerFactory.getLogger(LogCaptureTraitShould.class)
+
+  def "verify missing events"() {
+    expect:
+    logged(not(aLog()
+        .withLevel(equalTo(INFO))
+        .withMessage(equalTo("missing message"))))
+  }
+
+  def "verify multiple events"() {
+    expect:
+    log.info("first message")
+    log.debug("second message")
+    logged(allOf(
+        aLog().withLevel(INFO).withMessage("first message"),
+        aLog().withLevel(DEBUG).withMessage("second message"))
+    )
+  }
+
+  def "verify sync logs using rule"() {
+    expect:
+    log.info("a message")
+
+    logged(aLog().info().withMessage("a message"))
+  }
+
+}
